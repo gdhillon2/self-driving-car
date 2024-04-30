1e6ea94f35986aec98b7f2a0ac854ccaa4ca1d00
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-30 16:46:51 -0700

    fix: header file

--- main.c
+++ main.c
@@ -1,37 +1,29 @@
-#include "lib/MotorController/MotorController.h"
-#include "lib/MotorTest/MotorTest.h"
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "main.h"
 int main() {
   // initialize the motor information structures
-  motor_info* motors = malloc(sizeof(motor_info) * MOTOR_NUM);
-  
+  motor_info *motors = malloc(sizeof(motor_info) * MOTOR_NUM);
+
   Init_Motor(&motors[MOTOR_A], FORWARD, 100, PWMA, AIN1, AIN2, MOTORHAT_1);
   Init_Motor(&motors[MOTOR_B], FORWARD, 100, PWMB, BIN1, BIN2, MOTORHAT_1);
   Init_Motor(&motors[MOTOR_C], FORWARD, 100, PWMA, AIN1, AIN2, MOTORHAT_2);
   Init_Motor(&motors[MOTOR_D], FORWARD, 100, PWMB, BIN1, BIN2, MOTORHAT_2);
 
-/*****************************************************
-*  MOTOR TEST FUNCTIONS BELOW - UNCOMMENT AS NEEDED
-*****************************************************/
+  /*****************************************************
+   *  MOTOR TEST FUNCTIONS BELOW - UNCOMMENT AS NEEDED
+   *****************************************************/
 
-//  Test_Individual_Motor(&motors[MOTOR_A]);
-//  Test_Individual_Motor(&motors[MOTOR_B]);
-//  Test_Individual_Motor(&motors[MOTOR_C]);
-//  Test_Individual_Motor(&motors[MOTOR_D]);
+  //  Test_Individual_Motor(&motors[MOTOR_A]);
+  //  Test_Individual_Motor(&motors[MOTOR_B]);
+  //  Test_Individual_Motor(&motors[MOTOR_C]);
+  //  Test_Individual_Motor(&motors[MOTOR_D]);
   Test_Turn_Right(motors);
   Test_Turn_Left(motors);
-//  Test_Shift_Left(motors);
+  //  Test_Shift_Left(motors);
 
-/*****************************************************
-*  SENSOR TEST FUNCTIONS BELOW - UNCOMMENT AS NEEDED
-*****************************************************/
+  /*****************************************************
+   *  SENSOR TEST FUNCTIONS BELOW - UNCOMMENT AS NEEDED
+   *****************************************************/
 
-
-
-
-
-  free(motors);  
+  free(motors);
   return 0;
 }
--- main.h
+++ main.h
@@ -1,1 +1,7 @@
+#include "lib/MotorController/MotorController.h"
+#include "lib/MotorTest/MotorTest.h"
+#include "lib/SensorController/SensorController.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 extern volatile int running;
